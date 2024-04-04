86b931e5ead05f1bccc3cc112f629cc863bcebf5
0rion72 <138822284+0rion72@users.noreply.github.com>
2024-04-04 18:12:03 +0100

    Create Teen Number Checker.java

---
+++ Java from Udemy/Expressions, Statements & More/Teen Number Checker.java
@@ -0,0 +1,33 @@
+public class TeenNumberChecker 
+{
+    public static boolean hasTeen(int n1, int n2, int n3)
+    {
+        if (n1 >= 13 && n1 <= 19)
+        {
+            return true;
+        }
+        else if (n2 >= 13 && n2 <= 19)
+        {
+            return true;
+        }
+        else if (n3 >= 13 && n3 <= 19)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    public static boolean isTeen(int t1)
+    {
+        if (t1 >= 13 && t1 <= 19)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
