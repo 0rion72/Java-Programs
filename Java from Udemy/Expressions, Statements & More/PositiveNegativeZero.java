2da5ff2fb1b81d29262f11d07cfff71261f46c74
0rion72 <138822284+0rion72@users.noreply.github.com>
2024-04-04 04:36:37 +0100

    Update PositiveNegativeZero.java

--- Java from Udemy/Expressions, Statements & More/PositiveNegativeZero.java
+++ Java from Udemy/Expressions, Statements & More/PositiveNegativeZero.java
@@ -1,1 +1,19 @@
-
+public class PositiveNegativeZero 
+{
+    public static void checkNumber(int number)
+    {
+        if (number > 0)
+        {
+            System.out.println("positive");
+        }
+        else if (number < 0)
+        {
+            System.out.println("negative");
+        }
+        else
+        {
+            System.out.println("zero");
+        }
+        
+    }// write code here
+}
